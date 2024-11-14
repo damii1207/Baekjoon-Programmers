2b8dc30ef37fd57961cc9eb70785682e5f922e6c
류다미 <137788846+damii1207@users.noreply.github.com>
2024-11-14 16:36:23 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,24 +1,11 @@
-n, m = map(int, input().split(' '))
-mat1 = [[0 for j in range(m)] for i in range(n)]
-mat2 = [[0 for j in range(m)] for i in range(n)]
-mat3 = [[0 for j in range(m)] for i in range(n)]
+n,m = map(int,input().split())
+arr1 = [list(map(int, input().split())) for _ in range(n)]
+arr2 = [list(map(int, input().split())) for _ in range(n)]
+arr = [[0 for _ in range(m)] for _ in range(n)]
 
 for i in range(n):
-    col = list(map(int, input().split()))
     for j in range(m):
-      mat1[i][j] = col[j] 
-
+        arr[i][j] = arr1[i][j] + arr2[i][j]
+ 
 for i in range(n):
-    col = list(map(int, input().split()))
-    for j in range(m):
-      mat2[i][j] = col[j]
-   
-    
-for row in range(n):
-    for col in range(m):
-        mat3[row][col] = mat1[row][col] + mat2[row][col]
-
-for row in range(n):
-    for col in range(m):
-        print(mat3[row][col], end=' ')
-    print()
+    print(*arr[i],sep=' ')

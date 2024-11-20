9aaf005c222bdc0ecfb528e2c407a702a68be15d
류다미 <137788846+damii1207@users.noreply.github.com>
2024-11-20 10:14:20 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,14 +1,18 @@
-N = int(input())
-
-group = 0
-for _ in range(N):
+n = int(input())
+count = 0
+for i in range(n):
     str = input()
-    cnt = 0
-    for i in range(len(str)-1):
-        if str[i]!= str[i+1]:
-            rstr = str[i+1:]
-            if rstr.count(str[i]) > 0:
-                cnt += 1
-    if cnt == 0:
-        group += 1
-print(group)
+    se = set()
+    before = ""
+    group = True
+    for char in str:
+        if char != before and char in se:
+            group = False 
+            break
+        se.add(char)
+        before = char
+
+    if group:
+        count += 1
+
+print(count)
